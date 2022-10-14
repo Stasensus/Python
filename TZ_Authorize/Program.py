4e6b5408693e124362099bca138f8744799eea60
Stasensus <stanislav@kireev.ru>
2022-10-14 20:18:40 +0300

    Задача выполнена

--- TZ_Authorize/Program.py
+++ TZ_Authorize/Program.py
@@ -1,37 +1,35 @@
-def check_password():
-    auth_password = str(input('Enter password: '))
-    if 3 < len(auth_password) < 33:
-        print('Password is correct.')
-        f = open ("c:\\Users\\USER\\Desktop\\Python\\TZ_Authorize\\loginpass.txt")
-        for line in f:
-            if line == (auth_login + ':' + auth_password + '\n'):
-                final_solution = 'Successful authorization'
-            else:
-                final_solution = 'Access denied'
-        print(final_solution)
-        f.close()
-    else:
-        print('Password should contain from 4 to 32 symbols')
-        check_password()
+# def check_password():
+#     auth_password = str(input('Enter password: '))
+#     if 3 < len(auth_password) < 33:
+#         f = open ("c:\\Users\\USER\\Desktop\\Python\\TZ_Authorize\\loginpass.txt")
+#         for line in f:
+#             if line == (auth_login + ':' + auth_password + '\n'):
+#                 final_solution = 'Successful authorization'
+#             else:
+#                 final_solution = 'Access denied'
+#         print(final_solution)
+#         f.close()
+#     else:
+#         print('Password should contain from 4 to 32 symbols')
+#         check_password()
 
-def check_login():
+def check_loginpass():
     auth_login = str(input('To authorize enter login: '))
-    if 2 < len(auth_login) < 21:
-        f = open ("c:\\Users\\USER\\Desktop\\Python\\TZ_Authorize\\loginpass.txt")
-        for line in f:
-            if auth_login in line:
-                check_password()
-            else:
-                print('Login not found') 
-        f.close()      
-    else:
-        print("Login should contain from 3 to 20 symbols")
-        check_login()
-
+    auth_password = str(input('Enter password: '))
+    f = open ("c:\\Users\\USER\\Desktop\\Python\\TZ_Authorize\\loginpass.txt")
+    for line in f:
+        if line == (auth_login + ':' + auth_password + '\n'):
+            final_solution = 'Successful authorization'
+            break
+        else:
+            final_solution = 'Access denied. Login or password not found.'
+    f.close()
+    print(final_solution)
+        
 def check_regpassword():
     reg_password = str(input('Create password: '))
     if 3 < len(reg_password) < 33:
-        print('Password is correct')
+        print('Password is correct. You have registered sucessfully.')
         f = open("c:\\Users\\USER\\Desktop\\Python\\TZ_Authorize\\loginpass.txt", "a")
         f.write(reg_password + '\n')
         f.close()
@@ -54,7 +52,7 @@ def check_reglogin():
 def choice():
     answer = str(input('Do you want to authorize? Type "A". Or you want to register? Type "R": '))
     if answer.lower() == 'a':
-        check_login()
+        check_loginpass()
     elif answer.lower() == 'r': 
         check_reglogin()
     else:
